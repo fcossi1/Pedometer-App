74808b103a2f67b7f414c3b22a621ec821543bc7
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2022-05-17 19:44:24 -0400

    First Commit. Device reads steps and displayed on app. Implemented function that calculates calories burnt.

--- packages/pedometer/example/lib/main.dart
+++ packages/pedometer/example/lib/main.dart
@@ -19,18 +19,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late Stream<StepCount> _stepCountStream;
   late Stream<PedestrianStatus> _pedestrianStatusStream;
-  String _status = '?', _steps = '?';
+
+  Duration duration = Duration();
+  late Timer timer;
+  late TextEditingController textEditingController;
+
+  String _status = '?', _steps = '?', _cals = '?';
+  int countSec = 0;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
   }
 
   void onStepCount(StepCount event) {
     print(event);
     setState(() {
       _steps = event.steps.toString();
+      _cals = (calculateCals(int.parse(_steps), 175)) as String;
     });
   }
 
@@ -38,11 +52,15 @@ class _MyAppState extends State<MyApp> {
     print(event);
     setState(() {
       _status = event.status;
+      /*if (_status == 'walking') {
+        stopTimer();
+      }*/
     });
   }
 
   void onPedestrianStatusError(error) {
     print('onPedestrianStatusError: $error');
+    stopTimer();
     setState(() {
       _status = 'Pedestrian Status not available';
     });
@@ -51,8 +69,27 @@ class _MyAppState extends State<MyApp> {
 
   void onStepCountError(error) {
     print('onStepCountError: $error');
+    stopTimer();
     setState(() {
       _steps = 'Step Count not available';
+    });
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (timerCountUp));
+  }
+
+  void timerCountUp(_) {
+    setState(() {
+      final seconds = duration.inSeconds + 1;
+      duration = Duration(seconds: seconds);
+    });
+  }
+
+  void stopTimer() {
+    duration = Duration();
+    setState(() {
+      timer.cancel();
     });
   }
 
@@ -64,8 +101,19 @@ class _MyAppState extends State<MyApp> {
 
     _stepCountStream = Pedometer.stepCountStream;
     _stepCountStream.listen(onStepCount).onError(onStepCountError);
+    startTimer();
 
     if (!mounted) return;
+  }
+
+  double calculateCals(int _steps, int weight) {
+    // return (0.045 * _steps); <-- Calculate calories burnt without weight & height involved
+    double M = 0.1 * ((_steps / 1312.3359801) / duration.inSeconds) +
+        3.5; // Calculate the walking metabolic average
+    return 5 *
+        duration.inSeconds *
+        ((M * weight) /
+            1000); // Calculate the total burn of calories using metabolic avg
   }
 
   @override
@@ -86,6 +134,19 @@ class _MyAppState extends State<MyApp> {
               Text(
                 _steps,
                 style: TextStyle(fontSize: 60),
+              ),
+              Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              TextField(
+                controller: textEditingController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Your Weight',
+                ),
               ),
               Divider(
                 height: 100,
